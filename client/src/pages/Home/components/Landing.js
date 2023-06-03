4be7b7185c809d0a88f69b3ebe9ab01a82fee535
Tommy Hoang <tommyyhoangg@gmail.com>
2023-06-03 14:44:42 +0200

    Added nav for desktop

--- client/src/pages/Home/components/Landing.js
+++ client/src/pages/Home/components/Landing.js
@@ -8,28 +8,35 @@ const Landing = ({ langCode, lang }) => {
   const slicePosition = langCode === 'de' ? 2 : 3; // If its EN slice the title at a different position due to different lengths
 
   return (
-    <main className={css['landing']}>
-      <h3>{l.sub}</h3>
-      <h1>
-        <span>{l.title.split(' ').slice(0, slicePosition).join(' ')}</span>
-        <span>
-          {' '}
-          {l.title.split(' ').slice(slicePosition, l.title.length).join(' ')}
-        </span>
-      </h1>
-
-      <Link to={`/${langCode}/`} className={css['cta']}>
-        <div className={css['cta-main']}>
-          <img src={ChevronSVG} alt='chevron' />
+    <div className={css['cont']}>
+      <main className={css['landing']}>
+        <div>
+          <h3>{l.sub}</h3>
+          <h1>
+            <span>{l.title.split(' ').slice(0, slicePosition).join(' ')}</span>
+            <span>
+              {' '}
+              {l.title
+                .split(' ')
+                .slice(slicePosition, l.title.length)
+                .join(' ')}
+            </span>
+          </h1>
         </div>
 
-        <div className={css['cta-bubble']}>
-          <h4>{l.cta}</h4>
+        <Link to={`/${langCode}/`} className={css['cta']}>
+          <div className={css['cta-main']}>
+            <img src={ChevronSVG} alt='chevron' />
+          </div>
 
-          <div className={css['cta-dot']}></div>
-        </div>
-      </Link>
-    </main>
+          <div className={css['cta-bubble']}>
+            <h4>{l.cta}</h4>
+
+            <div className={css['cta-dot']}></div>
+          </div>
+        </Link>
+      </main>
+    </div>
   );
 };
 
