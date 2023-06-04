4e86361ef0d089a6a85b1f94afa9cdbd894e0fa9
Tommy Hoang <tommyyhoangg@gmail.com>
2023-06-04 15:43:02 +0200

    Added why us desktop

--- client/src/pages/Home/components/WhyUs.js
+++ client/src/pages/Home/components/WhyUs.js
@@ -9,17 +9,19 @@ const WhyUs = ({ langCode, lang }) => {
   const l = lang.why;
 
   return (
-    <section className={css['why']}>
-      <h3>{l.sub}</h3>
-      <h2>{l.title}</h2>
+    <div className={css['cont']}>
+      <section className={css['why']}>
+        <h3>{l.sub}</h3>
+        <h2>{l.title}</h2>
 
-      <div className={css['reasons']}>
-        <Reason img={Why1SVG} title={l.why1.title} text={l.why1.text} />
-        <Reason img={Why2SVG} title={l.why2.title} text={l.why2.text} />
-        <Reason img={Why3SVG} title={l.why3.title} text={l.why3.text} />
-        <Reason img={Why4SVG} title={l.why4.title} text={l.why4.text} />
-      </div>
-    </section>
+        <div className={css['reasons']}>
+          <Reason img={Why1SVG} title={l.why1.title} text={l.why1.text} />
+          <Reason img={Why2SVG} title={l.why2.title} text={l.why2.text} />
+          <Reason img={Why3SVG} title={l.why3.title} text={l.why3.text} />
+          <Reason img={Why4SVG} title={l.why4.title} text={l.why4.text} />
+        </div>
+      </section>
+    </div>
   );
 };
 
