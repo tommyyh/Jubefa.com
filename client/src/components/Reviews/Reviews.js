1125b76ecbfafa9b5018b9a66a336b592e443fe9
Tommy Hoang <tommyyhoangg@gmail.com>
2023-06-04 16:13:55 +0200

    Added reviews for desktop

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -3,18 +3,36 @@ import css from './reviews.module.scss';
 import Review from './components/Review';
 import Attestant1 from 'assets/images/attestant1.jpg';
 import Anonymous from 'assets/images/anonymous.jpg';
+import Cta from 'components/Cta/Cta';
+import variablesJson from 'data/variables/variables.json';
 
 const Reviews = ({ lang, langCode }) => {
   const l = lang.reviews;
+  const color = variablesJson.colors.accentPurple;
 
   return (
-    <section className={css['reviews']}>
-      <div className={css['list']}>
-        <Review l={l.review1} img={Anonymous} />
-        <Review l={l.review2} img={Anonymous} />
-        <Review l={l.review3} img={Anonymous} />
-      </div>
-    </section>
+    <div className={css['cont']}>
+      <section className={css['reviews']}>
+        <div className={css['list']}>
+          <Review l={l.review1} img={Anonymous} />
+          <Review l={l.review2} img={Anonymous} />
+          <Review l={l.review3} img={Anonymous} />
+          <Prompt l={l.prompt} langCode={langCode} color={color} />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const Prompt = ({ l, langCode, color }) => {
+  return (
+    <div className={css['prompt']}>
+      <h4>{l.sub}</h4>
+      <h3>{l.title}</h3>
+      <p>{l.text}</p>
+
+      <Cta title={l.cta} link={`/${langCode}/`} color={color} />
+    </div>
   );
 };
 
