9bc77fb23c44bc065fe33d58329d1c46c8473367
Tommy Hoang <tommyyhoangg@gmail.com>
2023-06-05 21:16:35 +0200

    Changes

--- jubefa/settings.py
+++ jubefa/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6@q+tj&7(za40@lrqv6&252(*_47lyn$8+k(#(*yta$cn5nd6!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
